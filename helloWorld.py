f2783787253bef24f09b88bbb2482b21c29c7ba1
Ubuntu <ubuntu@ip-172-31-42-232.ec2.internal>
2020-11-20 17:58:29 +0000

    commit

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by Tong Gong
+# Created time Novemver 2020
+# This is the Hello World program with nice format.
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
